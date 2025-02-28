6b71479f30abd03735b5f2587bcc13d372aba871
LaRita Robinson <larita@scientist.com>
2025-02-28 01:31:12 -0500

    Rubocop doesn't like unused params

--- app/helpers/shared_search_helper_decorator.rb
+++ app/helpers/shared_search_helper_decorator.rb
@@ -3,7 +3,7 @@
 # Override Hyku 6.0.x to remove the persistent search & highlighting
 # until we can ensure consistent behavior
 module SharedSearchHelperDecorator
-  def append_query_params(url, model, params)
+  def append_query_params(url, _model, _params)
     url
   end
 end
