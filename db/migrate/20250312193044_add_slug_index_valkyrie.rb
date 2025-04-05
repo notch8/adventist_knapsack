ae0e27ebb2f741dbc3fbd0880067e883aa032a28
Rob Kaufman <rob@notch8.com>
2025-04-04 21:32:54 -0700

    faster slug indexing

--- db/migrate/20250312193044_add_slug_index_valkyrie.rb
+++ db/migrate/20250312193044_add_slug_index_valkyrie.rb
@@ -1,8 +1,9 @@
 class AddSlugIndexValkyrie < ActiveRecord::Migration[6.1]
   def change
-    add_index :orm_resources,
-      "(metadata -> 'slug' ->> 0)",
-      name: 'index_on_slug',
+    # add index to orm_resources table for json field metadata subfield slug first element if the first element is not null
+    add_index :orm_resources, "(metadata->'slug'->>0)",
+      name: "index_orm_resources_on_metadata_slug_0",
       where: "metadata -> 'slug' IS NOT NULL"
+
   end
 end
