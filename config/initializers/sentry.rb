e45207434a61f158e3c61b0c43dc7b7838245ace
April Rieger <april.rieger@yahoo.com>
2025-06-05 19:27:24 -0700

    Remove sentry_logger to prevent circular error

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,7 @@ require 'sentry-sidekiq'
 
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger, :sentry_logger]
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.enabled_environments = %w[adv-knapsack-staging adv-knapsack-production]
   # Skip Hyrax 404s
   config.excluded_exceptions += ['Hyrax::ObjectNotFoundError']
